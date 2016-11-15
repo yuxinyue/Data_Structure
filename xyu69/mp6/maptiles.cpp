253e8e51a2994061cdeed794815118d81bad2e6b
xyu69 <xyu69@574e2964-c1e2-4f07-b934-cb5160de0139>
2016-11-15 01:56:43 +0000

    mp6 submission

--- xyu69/mp6/maptiles.cpp
+++ xyu69/mp6/maptiles.cpp
@@ -15,6 +15,35 @@ MosaicCanvas* mapTiles(SourceImage const& theSource,
     /**
      * @todo Implement this function!
      */
-    return NULL;
+    vector<Point<3>> vec;
+    map<RGBAPixel, TileImage> mapforTiles;
+    for(int i = 0; i < (int)theTiles.size(); i++){
+    	RGBAPixel color = theTiles[i].getAverageColor();
+ 
+    	Point<3> tilePoint(color.red, color.green, color.blue);
+    	vec.push_back(tilePoint);
+
+    	mapforTiles[color] = theTiles[i];
+    }
+
+   	KDTree<3> kdt(vec);
+
+   	int x = theSource.getRows();
+   	int y = theSource.getColumns();
+   	MosaicCanvas* mCanvas = new MosaicCanvas(x, y);
+
+   	for(int j = 0; j < x; j++){
+   		for(int k = 0; k < y; k++){
+   			RGBAPixel color = theSource.getRegionColor(j, k);
+   			Point<3> sourcePoint(color.red, color.green, color.blue);
+   			Point<3> resultPoint = kdt.findNearestNeighbor(sourcePoint);
+   			
+   			RGBAPixel resultPixel(resultPoint[0], resultPoint[1], resultPoint[2]);
+   			TileImage image = mapforTiles[resultPixel];
+
+   			mCanvas->setTile(j, k, image);
+   		}
+   	}
+   	return mCanvas;
 }
 

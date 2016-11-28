676d0497bfab838b730602e89b7251f8366c69ed
xyu69 <xyu69@574e2964-c1e2-4f07-b934-cb5160de0139>
2016-11-28 17:54:59 +0000

    mp7.1 submission

--- mp7/dsets.cpp
+++ mp7/dsets.cpp
@@ -1,1 +1,28 @@
 /* Your code here! */
+#include "dsets.h"
+
+void DisjointSets::addelements(int num){
+	for(int i = 0; i < num; i++){
+	vec.push_back(-1);
+	}
+}
+
+int DisjointSets::find(int elem){
+	if(vec[elem] < 0) return elem;
+	return vec[elem] = find(vec[elem]);
+}
+
+void DisjointSets::setunion(int a, int b){
+	int root1 = find(a);
+	int root2 = find(b);
+	int size = vec[root1] + vec[root2];
+//second points to first
+	if(vec[root1] <= vec[root2]){
+		vec[root2] = root1;
+		vec[root1] = size;
+	}
+	else{
+		vec[root1] = root2;
+		vec[root2] = size;
+	}
+}
--- mp7/dsets.h
+++ mp7/dsets.h
@@ -1,1 +1,24 @@
 /* Your code here! */
+#ifndef DISJOINTSETS_H 
+#define DISJOINTSETS_H
+
+#include <vector>
+using namespace std;
+
+class DisjointSets{
+	public:
+		void addelements(int num);
+		int find(int elem);
+		void setunion(int a, int b);			
+	private:
+		vector<int> vec;
+};
+
+#endif
+
+
+
+
+
+
+

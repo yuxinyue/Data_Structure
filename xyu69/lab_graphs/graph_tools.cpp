6e8611418470a526fe2f361d33b41695580b5506
xyu69 <xyu69@574e2964-c1e2-4f07-b934-cb5160de0139>
2016-12-04 04:57:16 +0000

    lab_graphs submission

--- xyu69/lab_graphs/graph_tools.cpp
+++ xyu69/lab_graphs/graph_tools.cpp
@@ -26,7 +26,24 @@
 int GraphTools::findMinWeight(Graph& graph)
 {
     /* Your code here! */
-    return -1;
+
+    initialization(graph);
+	int min = 100000;
+	Vertex min_point_1;
+	Vertex min_point_2;
+
+    vector<Edge> vec_edge = graph.getEdges();
+    vector<Vertex> vec_vertex = graph.getVertices();
+
+    for(auto & v : vec_edge){
+    	if(v.weight < min){
+    		min = v.weight;
+    		min_point_1 = v.source;
+    		min_point_2 = v.dest;
+    	}
+    }   
+    graph.setEdgeLabel(min_point_1, min_point_2, "MIN");
+    return min;
 }
 
 /**
@@ -53,7 +70,46 @@ int GraphTools::findMinWeight(Graph& graph)
 int GraphTools::findShortestPath(Graph& graph, Vertex start, Vertex end)
 {
     /* Your code here! */
-    return -1;
+    /*initial*/
+   initialization(graph);
+    /*Traversal*/
+    queue<Vertex> q;
+    graph.setVertexLabel(start, "VISITED");
+    q.push(start);
+    unordered_map<Vertex, int> distance;
+    distance[start] = 0;
+    int dis;
+    Vertex find;
+    unordered_map<Vertex, Vertex> preVertex;
+
+    while(! q.empty()){
+    	Vertex orig_v = q.front();
+    	q.pop();
+    	vector<Vertex> adj = graph.getAdjacent(orig_v);
+        for(auto & a : adj){
+            if(graph.getVertexLabel(a) == "UNEXPLORED"){
+                graph.setVertexLabel(a, "VISITED");
+                graph.setEdgeLabel(orig_v, a, "DISCOVERY");
+                q.push(a);
+                distance[a] = distance[orig_v] + 1;
+                preVertex[a] = orig_v;
+            }
+            else if(graph.getEdgeLabel(orig_v, a) == "UNEXPLORED"){
+                graph.setEdgeLabel(orig_v, a, "CROSS");
+            }
+            if(a == end){
+                dis = distance[a];
+                break;
+            }
+        }
+    }
+    find = end;
+    while(find != start){
+        Vertex pre = preVertex[find];
+        graph.setEdgeLabel(pre, find, "MINPATH");
+        find = pre;
+    }    
+    return dis;
 }
 
 /**
@@ -72,5 +128,36 @@ int GraphTools::findShortestPath(Graph& graph, Vertex start, Vertex end)
 void GraphTools::findMST(Graph& graph)
 {
     /* Your code here! */
+    initialization(graph);
+    vector<Edge> vec_edge = graph.getEdges();
+    vector<Vertex> vec_vertex = graph.getVertices();
+    sort(vec_edge.begin(), vec_edge.end());
+    DisjointSets set;
+    set.addelements( (int)vec_vertex.size() );
+    int counts = 0;
+    for(auto & e : vec_edge){
+        if(set.find(e.source) != set.find(e.dest)){
+            set.setunion(e.source, e.dest);
+            graph.setEdgeLabel(e.source, e.dest, "MST");
+            counts++;
+        }
+        if(counts >= (int)vec_vertex.size()-1)
+            break;
+    }
 }
 
+
+void GraphTools::initialization(Graph & graph){
+
+    vector<Edge> vec_edge = graph.getEdges();
+    vector<Vertex> vec_vertex = graph.getVertices();
+
+    for(auto & v : vec_vertex){
+        graph.setVertexLabel(v, "UNEXPLORED");
+    }
+    
+    for(auto & e : vec_edge){
+        graph.setEdgeLabel(e.source, e.dest, "UNEXPLORED");
+    }
+
+}
